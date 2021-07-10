635afa536a48535641f7644f6ebba03853a0b825
Monuar Hossain <monuar.cste@gmail.com>
2021-07-10 20:39:33 +0600

    initial commit

--- Bit Manipulation/Leetcode/count_the_number_of_consistent_string.cpp
+++ Bit Manipulation/Leetcode/count_the_number_of_consistent_string.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int countConsistentStrings(string s, vector<string>& v) {
+        int cnt=v.size();
+        map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        for(int i=0;i<v.size();i++){
+            for( auto c:v[i]){
+                if(mp.find(c)==mp.end()){
+                    cnt--;
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+};
