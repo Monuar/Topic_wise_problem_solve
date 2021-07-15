ba3486779666c3527ddccd15a87ae48872caf97a
Monuar Hossain <monuar.cste@gmail.com>
2021-07-15 11:21:46 +0600

    initial commit

---
+++ Two Pointer/LeetCode/Flipping_an_image.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& v) {
+       for(int i=0;i<v.size();i++){
+           reverse(v[i].begin(),v[i].end());
+       }
+        for(int i=0;i<v.size();i++){
+            for(int j=0;j<v[i].size();j++){
+                v[i][j]^=1;
+            }
+        }
+        return v;
+    }
+};
