5dd590556208449dca40a7932527661b34892547
Monuar Hossain <monuar.cste@gmail.com>
2021-07-26 21:50:04 +0600

    initial commit

---
+++ Number_theory/hackerearth/euler_totient_function.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void phi(ll n)
+{
+    ll ans=n;
+    for(ll i=2;i*i<=n;i++){
+        if(n%i==0){
+            while(n%i==0){
+                n/=i;
+            }
+            ans=ans*(i-1);
+            ans/=i;
+        }
+    }
+    if(n>1) {
+        ans*=(n-1);
+        ans/=n;
+    }
+    cout<<ans<<endl;
+}
+int main()
+{
+    ll n;
+    cin>>n;
+    phi(n);
+}
