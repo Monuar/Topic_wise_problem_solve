94220ac30525a66d30318df81d83f19fbba94efc
Monuar Hossain <monuar.cste@gmail.com>
2021-07-29 16:12:16 +0600

    initial commit

--- Two Pointer/codeforces/boat_competiion.cpp
+++ Two Pointer/codeforces/boat_competiion.cpp
@@ -11,7 +11,7 @@ int main()
         int a[n];
         for(int i=0; i<n; i++) cin>>a[i];
         sort(a,a+n);
-        for(int i=2; i<2*a[n-1]; i++)
+        for(int i=2; i<=2*a[n-1]; i++)
         {
             int l=0,r=n-1;
             int cnt=0;
