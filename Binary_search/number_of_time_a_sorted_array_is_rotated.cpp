41b5d44d78e94c2e71ee51a81637b51629b988fa
Monuar Hossain <monuar.cste@gmail.com>
2021-08-27 13:26:25 +0600

    initial commit

--- Binary_search/number_of_time_a_sorted_array_is_rotated.cpp
+++ Binary_search/number_of_time_a_sorted_array_is_rotated.cpp
@@ -1,31 +1,43 @@
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
-void bs(ll a[],ll n)
-{
-    ll l=0,r=n-1,ans=0;
-    while(l<=r)
-    {
-        ll mid=(l+r)/2;
-        if(a[mid+1]<a[mid]&&mid<r){
-            ans=mid+1;
-            break;
-        }
-        else if(a[mid-1]>a[mid]&&l<mid){
-            ans=mid;
-            break;
-        }
-        if(a[mid]<a[r]) r=mid-1;
-        else l=mid+1;
+class Solution {
+public:
+    int findMin(vector<int>& a) {
+        int l=0,r=a.size()-1;
+        int cnt=0;
+        while(l<=r)
+             {
+                int mid=l+(r-l)/2;
+                if(mid<r){
+                    if(a[mid+1]<a[mid]){
+                    cnt=mid+1;
+                    break;
+                    }
+                }
+                 if(l<mid){
+                     if(a[mid-1]>a[mid]){
+                    cnt=mid;
+                    break;
+                     }
+                    }
+             if(a[mid]<a[r]) r=mid-1;
+                else l=mid+1;
 
-    }
-    cout<<ans<<endl;
-}
+          }
+         return a[cnt];
+        }
+
+};
 int main()
 {
     ll n;
     cin>>n;
     ll a[n];
     for(ll i=0;i<n;i++) cin>>a[i];
-    bs(a,n);
+    Solution sln;
+    vector<int> vec;
+    vec.assign(a, a+n);
+    int res=sln.findMin(vec);
+    cout<<(res);
 }
